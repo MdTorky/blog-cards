44f432a651b767274852c6f65b3e4d638b7e2c32
cargo <mohamed2003torky@gmail.com>
2024-04-05 02:04:26 +0200

    Finished The Cards

--- src/App.js
+++ src/App.js
@@ -23,20 +23,18 @@ function App() {
 
     fetchData();
   }, []);
-
   return (
     <div className="App">
       <div className="containers">
         {posts.map(post => (
           <div className="container" key={post.id}>
-            <h2 className='container-title'>Cloud And Service</h2>
+            <h2 className='container-title'>Cloud And Server</h2>
             <hr />
             <img src={post?.featured_media} className="" />
             <h3>{post?.title.rendered}</h3>
             <p>By <a href={post?._embedded.author[0].url}> {post._embedded.author[0].name}</a> on {formatDate(post.date)}</p>
             <hr />
             <h4>Article</h4>
-            {/* <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} /> */}
           </div>
         ))}
       </div>
